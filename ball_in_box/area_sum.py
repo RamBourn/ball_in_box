4d3e4dedf5538f3b3ad2f2e78d531fb252e4e9e0
RamBourn <35801144+RamBourn@users.noreply.github.com>
2018-06-01 19:21:18 +0800

    Update area_sum.py
    加入了一些中文注释，使得代码更加易读

--- ball_in_box/area_sum.py
+++ ball_in_box/area_sum.py
@@ -2,7 +2,7 @@ import math
 import ball_in_box.ballinbox as bb
 
 
-def area_sum(circles):
+def area_sum(circles):    #面积和
     area = 0.0
     for circle in circles:
         area += circle[2]**2 * math.pi
@@ -11,12 +11,12 @@ def area_sum(circles):
 
 if __name__ == '__main__':
     num_of_circle = 5
-    blockers = [(0.5, 0.5)
+    blockers = [(0.5, 0.5)     #障碍物的位置
                ,(0.5, -0.3)]
     
     circles = bb.ball_in_box(num_of_circle, blockers)
     
-    if num_of_circle == len(circles) :
+    if num_of_circle == len(circles) :    #圆的个数
         area = area_sum(circles)
         print("Total area: {}".format(area))
     else:
